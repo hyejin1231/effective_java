a3824ea114ada335b21df0630adcf7ebeb3de577
hyejin1231 <phj981231@naver.com>
2023-03-06 16:52:30 +0900

    [20230306] item20. 추상클래스보다는 인터페이스를 우선하라. 완벽공략 - 디폴트 메서드와 Object 메서드

---
+++ src/main/java/chapter03/item20/objectmethod/MyInterface.java
@@ -0,0 +1,24 @@
+package chapter03.item20.objectmethod;
+
+/**
+ * item20.추상클래스보다 인터페이스를 우선하라.
+ * 완벽공략 - 디폴트 메서드와 Object 메서드
+ * : 디폴트 메서드는 equals, hashCode, toString같은 Object 메서드를 재정의할 수 없다.
+ */
+public interface MyInterface
+{
+//	default String toString()
+//	{
+//		return "myString";
+//	}
+//
+//	default int hashCode()
+//	{
+//		return 10;
+//	}
+//
+//	default boolean equals(Object o)
+//	{
+//		return true;
+//	}
+}
